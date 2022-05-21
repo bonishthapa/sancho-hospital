9bc69ea0a967f85aeb2d92d1687fa0ddff571cb3
bonishthapa <bonishthapa@gmail.com>
2022-05-21 23:43:29 +0545

    doctor view page created

--- config/staff/views.py
+++ config/staff/views.py
@@ -1,3 +1,9 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
+from staff.models import Doctor
+
 # Create your views here.
+class DoctorListView(ListView):
+    model = Doctor
+    template_name = 'doctor-list.html'
