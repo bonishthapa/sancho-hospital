2276508cec99341768d55c1999b53c6cc2b73cf2
bonishthapa <bonishthapa@gmail.com>
2022-05-21 17:00:35 +0545

    url name changed from home to index

--- config/hospital/urls.py
+++ config/hospital/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from hospital.views import HomePageView
 
 urlpatterns =[
-    path('',HomePageView.as_view(),name="home"),
+    path('',HomePageView.as_view(),name="index"),
 ]
