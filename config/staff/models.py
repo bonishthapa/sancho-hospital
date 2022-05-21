75716f62865fd2c82b0c87499cc13299f0fc5452
bonishthapa <bonishthapa@gmail.com>
2022-05-21 22:33:35 +0545

    media file delete for media change and object delete

--- config/staff/models.py
+++ config/staff/models.py
@@ -1,4 +1,7 @@
+import os
+
 from django.db import models
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -12,9 +15,41 @@ class Doctor(models.Model):
     number = models.IntegerField()
     address = models.CharField(max_length=250)
     qualification = models.CharField(choices=education,max_length=250)
+    photo = models.ImageField(upload_to="doctor", null=True,blank=True)
 
     def __str__(self):
         return self.name
+
+
+@receiver(models.signals.post_delete, sender=Doctor)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes photo from filesystem
+    when corresponding `Doctor` object is deleted.
+    """
+    if instance.photo:
+        if os.path.isfile(instance.photo.path):
+            os.remove(instance.photo.path)
+
+@receiver(models.signals.pre_save, sender=Doctor)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old photo from filesystem
+    when corresponding `Doctor` object is updated
+    with new photo.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = Doctor.objects.get(pk=instance.pk).photo
+    except Doctor.DoesNotExist:
+        return False
+
+    new_file = instance.photo
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
 
 
 class Nurse(models.Model):
@@ -27,6 +62,37 @@ class Nurse(models.Model):
     number = models.IntegerField()
     address = models.CharField(max_length=250)
     qualification = models.CharField(choices=education,max_length=250)
+    photo = models.ImageField(upload_to="nurse", null=True,blank=True)
 
     def __str__(self):
         return self.name        
+
+@receiver(models.signals.post_delete, sender=Nurse)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes photo from filesystem
+    when corresponding `Nurse` object is deleted.
+    """
+    if instance.photo:
+        if os.path.isfile(instance.photo.path):
+            os.remove(instance.photo.path)
+
+@receiver(models.signals.pre_save, sender=Nurse)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `Nurse` object is updated
+    with new photo.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = Nurse.objects.get(pk=instance.pk).photo
+    except Nurse.DoesNotExist:
+        return False
+
+    new_file = instance.photo
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
