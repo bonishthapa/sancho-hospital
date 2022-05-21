a1f2df563edfb520fddb88c651d094de9a2c72df
bonishthapa <bonishthapa@gmail.com>
2022-05-21 23:22:27 +0545

    two context passed

--- config/hospital/views.py
+++ config/hospital/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+
 from hospital.models import Banner
+from staff.models import Doctor, Nurse
 
 # Create your views here.
 
@@ -8,9 +10,14 @@ class HomePageView(ListView):
     model = Banner
     template_name = "index.html"
 
-    # def get_context_data(self, **kwargs):
-    #     context={
-    #         "apple":"shau",
-    #         "mango":"aap"
-    #     }
-    #     return super().get_context_data(**context)
+    def get_context_data(self, **kwargs):
+        doctor = Doctor.objects.all()
+        nurse = Nurse.objects.all()
+        context={
+            "doctor":doctor,
+            "nurse":nurse
+        }
+        return super().get_context_data(**context)
+
+# def handler404(request):
+#     return render(request,'404.html')
