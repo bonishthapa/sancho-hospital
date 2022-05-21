2dc8c13f825d72b8a5fe17ff4450f935eb957266
bonishthapa <bonishthapa@gmail.com>
2022-05-21 23:22:47 +0545

    __pychace and migrations

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_lm36o=rft1)q60qy#aego^poew804=98*8dbfuf01w^+ky88q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
