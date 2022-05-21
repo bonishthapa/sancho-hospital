172c9bfd9e14874b5dc3540af9c7a83c302b4136
bonishthapa <bonishthapa@gmail.com>
2022-05-21 23:19:06 +0545

    department model added

--- config/hospital/models.py
+++ config/hospital/models.py
@@ -9,3 +9,10 @@ class Banner(models.Model):
 
     def __str__(self):
         return self.name
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=250)
+    department_description = models.TextField()
+
+    def __str__(self):
+        return self.department_name        
