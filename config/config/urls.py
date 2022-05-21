4b81db5c98ad85d006c7720f6aa5f4596733b051
bonishthapa <bonishthapa@gmail.com>
2022-05-21 23:42:35 +0545

    staff app url added

--- config/config/urls.py
+++ config/config/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('hospital.urls')),
+    path('',include('staff.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
